eb2abfd5d377f58838ca6236bead062ea6952b28
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-07-14 12:04:14 +0530

    Outputting a List of Recipes using ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is Test", "https://static01.nyt.com/images/2022/09/29/dining/afg-ricotta-polpette/merlin_213671208_2553d655-4170-4155-acdf-5fca2ce34a06-master768.jpg?w=1280&q=75"),
     new Recipe("A Test Recipe", "This is Test", "https://static01.nyt.com/images/2022/09/29/dining/afg-ricotta-polpette/merlin_213671208_2553d655-4170-4155-acdf-5fca2ce34a06-master768.jpg?w=1280&q=75")
+
   ];
 
 }
