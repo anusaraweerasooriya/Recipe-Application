363e805d9791b4c767947629a3cf46754db91ae8
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-07-15 22:33:59 +0530

    Used recipe service for Cross-Component Communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe("A Test Recipe", "This is Test", "https://static01.nyt.com/images/2022/09/29/dining/afg-ricotta-polpette/merlin_213671208_2553d655-4170-4155-acdf-5fca2ce34a06-master768.jpg?w=1280&q=75"),
         new Recipe("Another Test Recipe", "This is Test", "https://static01.nyt.com/images/2022/09/29/dining/afg-ricotta-polpette/merlin_213671208_2553d655-4170-4155-acdf-5fca2ce34a06-master768.jpg?w=1280&q=75")
